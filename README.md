3e295454dc63c5e526f74bcc57ab3fb565fadff0
JP Pienaar <jp@synthesis.co.za>
2022-09-07 09:13:22 +0200

    Working Application

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # springboot-postgres
+This repo contains the source code for a SprintBoot application that communicates with an PostgreSQL database hosted in EKS.
---
+++ springboot-rest-mysql/src/main/java/com/mycompany/springrestmysql/model/User.java
@@ -0,0 +1,34 @@
+package com.mycompany.springrestmysql.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+
+
+    public User (String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
